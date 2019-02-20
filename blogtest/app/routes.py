c414b97afecad6c5e1ca9fc39b80df0c43a7f333
egimoro <gimoremma@gmail.com>
2019-02-20 23:58:52 +0100

    indexroute1

--- blogtest/app/routes.py
+++ blogtest/app/routes.py
@@ -1,8 +1,9 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 
-def index():
-    return ('Jambo Bwana!')
-    
+def index():    
+    user={'username':'Lopingemma'}
+    return render_template('index.html',title='Home',user=user)
