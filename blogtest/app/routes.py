01ee15c82e547a2a6b1cc8f1a564d30c4c760aaf
egimoro <gimoremma@gmail.com>
2019-02-21 00:38:21 +0100

    mitted

--- blogtest/app/routes.py
+++ blogtest/app/routes.py
@@ -6,4 +6,14 @@ from flask import render_template
 
 def index():    
     user={'username':'Lopingemma'}
-    return render_template('index.html',title='Home',user=user)
+    posts=[
+        {
+            'author':{'username':'Klohn'},
+            'body':'Beautiful day in Portland!'
+        },
+        {
+            'author':{'username':'Dosan'},
+            'body':'The Avengers are stale!'
+        }
+    ]
+    return render_template('index.html',title='Home',user=user,posts=posts)
