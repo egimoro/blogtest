c32b781dc284ac1509f8fa8afed593f6251a25e5
egimoro <gimoremma@gmail.com>
2019-03-01 17:07:06 +0100

    parse

--- blogtest/app/__init__.py
+++ blogtest/app/__init__.py
@@ -15,4 +15,6 @@ migrate=Migrate(app,db)
 
 login=LoginManager(app)
 
+login.login_view='login'
+
 from app import routes,models 
--- blogtest/app/models.py
+++ blogtest/app/models.py
@@ -1,8 +1,9 @@
-from app import db
+from app import db,login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     id=db.Column(db.Integer,primary_key=True)
     username=db.Column(db.String(64),index=True,unique=True)
     email=db.Column(db.String(120),index=True,unique=True)
@@ -26,3 +27,8 @@ class Post(db.Model):
 
     def __ref__(self):
         return '<Post {}>'.format(self.body)
+@login.user_loader #Flask-Login keeps track of the logged in user by storing 
+                    #its unique identifier in Flask's user session, 
+                    # a storage space assigned to each user who connects to the application
+def load_user(id):
+    return User.query.get(int(id))
